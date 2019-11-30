302fa81950672ea80c1a7dc5180f76184a8c245e
George Kaimakas <gkaimakas@gmail.com>
2019-11-30 12:02:55 +0200

    Initial Commit

---
+++ Sudoku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sudoku
+//
+//  Created by George Kaimakas on 30/11/19.
+//  Copyright © 2019 George Kaimakas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
